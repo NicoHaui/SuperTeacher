fee8abfd134979e56bc8d892b54b04d9e8606c8d
Samuel Dolt <samuel@dolt.ch>
2016-03-08 14:11:26 +0100

    Test travis

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
  */
 int main(int, char**){
 	if (SDL_Init(SDL_INIT_VIDEO) != 0){
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		std::cout << "SDL init Error: " << SDL_GetError() << std::endl;
 		return 1;
 	}
 	SDL_Quit();
