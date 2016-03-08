d21bad713c1efd559ee670fc4d59dfa05c2af0f7
Samuel Dolt <samuel@dolt.ch>
2016-03-08 16:35:43 +0100

    Only build html doc in travis

--- docs/build.sh
+++ docs/build.sh
@@ -11,16 +11,11 @@ cp *.adoc build/
 
 cd build
 
-# Convert PDF to PNG
-find . -type f -name '*.pdf' -print0 |
-  while IFS= read -r -d '' file
-    do convert -verbose -density 150 -rotate 90 "${file}" "${file%.*}.png"
-  done
 
-# Create EC_DS2450.html
+# Create  rapport.html
 asciidoc --backend html5 --theme flask -a toc2 -a icons --section-number rapport.adoc 
 
-# Create EC_DS2450.pdf
+# Create rapport.pdf
 a2x --no-xmllint -f pdf -d article --dblatex-opts "-P doc.publisher.show=0 --param=latex.encoding=utf8"  rapport.adoc
 
 cd ..
