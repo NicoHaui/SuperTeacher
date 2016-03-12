add78ee3a22fca1d5c145d85d027ef7d155f141c
Samuel Dolt <samuel@dolt.ch>
2016-03-12 18:43:39 +0100

    Add GTest in travis osx

--- scripts/travis-osx-setup.sh
+++ scripts/travis-osx-setup.sh
@@ -2,4 +2,11 @@
 
 brew update &&
 brew install asciidoc &&
-brew install sdl2
+brew install sdl2 &&
+
+curl -O https://github.com/google/googletest/archive/release-1.7.0.zip &&
+unzip googletest-release-1.7.0.zip && cd googletest-release-1.7.0 &&
+mkdir build && cd build &&
+cmake .. && make &&
+sudo cp -r ../include/gtest /usr/local/include/ &&
+sudo cp lib*.a /usr/local/lib
