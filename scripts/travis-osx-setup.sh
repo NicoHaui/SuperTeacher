665e03aa1fce0e7fa6f567291c0b56ddbe08e754
Samuel Dolt <samuel@dolt.ch>
2016-03-12 18:50:03 +0100

    Fix GTest tarball dl in travis osx

--- scripts/travis-osx-setup.sh
+++ scripts/travis-osx-setup.sh
@@ -4,9 +4,10 @@ brew update &&
 brew install asciidoc &&
 brew install sdl2 &&
 
-curl -O https://github.com/google/googletest/archive/release-1.7.0.zip &&
-unzip googletest-release-1.7.0.zip && cd googletest-release-1.7.0 &&
+curl -L -O https://github.com/google/googletest/archive/release-1.7.0.zip &&
+unzip release-1.7.0.zip && cd googletest-release-1.7.0 &&
 mkdir build && cd build &&
 cmake .. && make &&
 sudo cp -r ../include/gtest /usr/local/include/ &&
-sudo cp lib*.a /usr/local/lib
+sudo cp lib*.a /usr/local/lib &&
+cd ../../
