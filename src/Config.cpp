e0cbbc63e13b406c5eb7c7f4a3cfcea2963f4329
Etienne Houriet <stoutche@gmail.com>
2016-04-26 08:37:45 +0200

    Revert "Config"
    This reverts commit 4eff404255a95311b717592ac42d0f894eca9829.

--- src/Config.cpp
+++
@@ -1,9 +0,0 @@
-#include "Config.h"
-
-void Config::Read(void)
-{
-	ResourceManager resource = {};
-	auto file_conf = resource.get_json("conf.json");
-
-	if ((bool)(*file_conf)["video"]["fullscreen"]) {
-}
