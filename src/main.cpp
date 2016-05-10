e6c3e0e3ad79f37346d0a79c3d1bb730db7909b5
Lars Aeschlimann <lars.aeschlimann@students.bfh.ch>
2016-05-10 09:48:50 +0200

    Plusieures modifications GUI et code

--- src/main.cpp
+++ src/main.cpp
@@ -86,14 +86,15 @@ int main(int argc, char *argv[]) {
     cloud_sprite->setTexture(*cloud_texture);
     cloud_sprite->move(200,200);
 
-    auto cactus_texture = resource->get_texture("graphics/tests/Items/cactus.png");
-    auto cactus_sprite = make_shared<sf::Sprite>();
-    cactus_sprite->setTexture(*cactus_texture);
-    cactus_sprite->setScale(4,2);
-    cactus_sprite->setTextureRect(sf::IntRect(0,0,BLOCK_PXSIZE * 3,BLOCK_PXSIZE * 3));
+    auto window_texture = resource->get_texture("graphics/backgrounds/window.png");
+    auto window_sprite = make_shared<sf::Sprite>();
+    window_sprite->setTexture(*window_texture);
+    window_sprite->setScale(0.6,0.6);
+    window_sprite->setTextureRect(sf::IntRect(0,0,BLOCK_PXSIZE * 9,BLOCK_PXSIZE * 14));
 
-    cactus_sprite->move(500, (ground_level - 6) * BLOCK_PXSIZE);
-	front_print.add_drawable(cactus_sprite);
+    window_sprite->move(500, (ground_level - 12) * BLOCK_PXSIZE);
+	background.add_drawable(window_sprite);
+    
 
     auto cloud2_sprite = make_shared<sf::Sprite>();
     cloud2_sprite->setTexture(*cloud_texture);
