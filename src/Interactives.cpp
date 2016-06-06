8aa473819e3f747b524c1201ea3e306bfd09e2e2
Etienne Houriet <stoutche@gmail.com>
2016-06-06 17:46:34 +0200

    A little progress
    Try to have universal platform collision

--- src/Interactives.cpp
+++ src/Interactives.cpp
@@ -63,8 +63,8 @@ Interactives::Interactives(std::shared_ptr<ResourceManager> resource, std::strin
         temp->move(x*BLOCK_PXSIZE, y*BLOCK_PXSIZE);
         temp->setScale(object["size"], object["size"]);
         sprite->sprite = temp;
-        sprite->sprite->setOrigin((sprite->sprite->getGlobalBounds().width / 2), (sprite->sprite->getGlobalBounds().height / 2));
-        sprite->sprite->move((sprite->sprite->getGlobalBounds().width / 2), (sprite->sprite->getGlobalBounds().height / 2));
+        //sprite->sprite->setOrigin((sprite->sprite->getGlobalBounds().width / 2), (sprite->sprite->getGlobalBounds().height / 2));
+        //sprite->sprite->move((sprite->sprite->getGlobalBounds().width / 2), (sprite->sprite->getGlobalBounds().height / 2));
         //add_drawable(sprite);
         sprite->function = string_conv(object["function"]);
         sprite->use = false;
@@ -74,78 +74,6 @@ Interactives::Interactives(std::shared_ptr<ResourceManager> resource, std::strin
     }
 }
 
-/*void Interactives::update(std::shared_ptr<Character> mainPerson, std::shared_ptr<sf::Text> score)
-{
-    
-    static float val = 0;
-    val+=0.2;
-    static int points = 0;
-    bool no_col = true;
-    for (auto pack : m_sprites)
-    {
-        if (pack->function == platform)
-        {
-            if (mainPerson->get_rectangle().intersects(pack->sprite->getGlobalBounds()))
-            {
-                if ((mainPerson->get_rectangle().top + mainPerson->get_rectangle().height) <= pack->sprite->getGlobalBounds().top)
-                {
-                    mainPerson->walk_level = pack->sprite->getGlobalBounds().top;
-                }
-                else if(mainPerson->get_rectangle().left+mainPerson->get_rectangle().width/2 <= pack->sprite->getGlobalBounds().left+pack->sprite->getGlobalBounds().width/2)
-                {
-                    mainPerson->left_enable = false;
-                }
-                else
-                {
-                    mainPerson->right_enable = false;
-                }
-                no_col = false;
-            }
-
-        }
-        if(pack->function == bonus)
-        {
-            if (mainPerson->get_rectangle().intersects(pack->sprite->getGlobalBounds()))
-            {
-                if (pack->use == false)
-                {
-                    auto texture = m_resource->get_texture("graphics/interactives/invisible.png");
-                    pack->sprite->setTexture(*texture);
-                    points++;
-                }
-                pack->use = true;
-            }
-            else
-            {
-                //pack.use = false;
-            }
-            
-        }
-        if (pack->function == mob)
-        {
-            pack->sprite->move(cos(val/5)*10, 0);
-            if (mainPerson->get_rectangle().intersects(pack->sprite->getGlobalBounds()))
-            {
-                if (pack->use == false)
-                {
-                    points--;
-                    //pack.sprite->setRotation(90);
-                }
-                pack->use = true;
-            }
-            else
-            {
-                pack->use = false;
-            }
-        }
-    }
-    if (no_col)
-    {
-        mainPerson->left_enable = true;
-        mainPerson->right_enable = true;
-    }
-    score->setString("Points: " + std::to_string(points));
-}*/
 
 colision Interactives::update( sf::FloatRect rect, std::shared_ptr<sf::Text> score)
 {
@@ -156,21 +84,30 @@ colision Interactives::update( sf::FloatRect rect, std::shared_ptr<sf::Text> sco
     bool no_col = true;
     for (auto pack : m_sprites)
     {
+        if (points == -5)
+        {
+            auto texture = m_resource->get_texture("graphics/interactives/point0.png");
+            pack->sprite->setTexture(*texture);
+        }
         if (pack->function == platform)
         {
             if (rect.intersects(pack->sprite->getGlobalBounds()))
             {
-                if ((rect.top + rect.height) <= pack->sprite->getGlobalBounds().top)
+                if ((rect.top ) <= pack->sprite->getGlobalBounds().top - pack->sprite->getGlobalBounds().height+10)
                 {
-                    col.walk_level = pack->sprite->getGlobalBounds().top;
+                    col.walk_level = pack->sprite->getGlobalBounds().top-pack->sprite->getGlobalBounds().height;
+                    //col.walk_level = 658 - (BLOCK_PXSIZE * ((SCREEN_Y_BLOCKS)-16));
+        points = 0;
                 }
                 else if (rect.left + rect.width / 2 >= pack->sprite->getGlobalBounds().left + pack->sprite->getGlobalBounds().width / 2)
                 {
                     col.left_enable = false;
+                    col.walk_level = 658 - (BLOCK_PXSIZE * ((SCREEN_Y_BLOCKS)-16));
                 }
                 else
                 {
                     col.right_enable = false;
+                    col.walk_level = 658 - (BLOCK_PXSIZE * ((SCREEN_Y_BLOCKS)-16));
                 }
                 no_col = false;
             }
@@ -190,7 +127,7 @@ colision Interactives::update( sf::FloatRect rect, std::shared_ptr<sf::Text> sco
             }
             else
             {
-                //pack.use = false;
+                pack->use = false;
             }
 
         }
@@ -212,10 +149,11 @@ colision Interactives::update( sf::FloatRect rect, std::shared_ptr<sf::Text> sco
             }
         }
     }
-    if (no_col)
+    if (no_col==true)
     {
         col.left_enable = true;
         col.right_enable = true;
+        col.walk_level = 658 - (BLOCK_PXSIZE * ((SCREEN_Y_BLOCKS)-16));
     }
     score->setString("Points: " + std::to_string(points));
     return col;
