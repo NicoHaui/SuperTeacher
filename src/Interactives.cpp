7a3b471792ebee7b5514be8767e70778d17b0695
Etienne Houriet <stoutche@gmail.com>
2016-06-06 18:15:52 +0200

    Resolve problem with platforms
    Now, a platform can be draw with json

--- src/Interactives.cpp
+++ src/Interactives.cpp
@@ -1,8 +1,8 @@
 //
-//  Background.cpp
+//  Interactives.cpp
 //  SuperTeacher
 //
-//  Created by Samuel Dolt on 03.05.16.
+//  Created by Etienne Houriet on 06.06.16.
 //
 //
 
@@ -34,42 +34,25 @@ Interactives::Interactives(std::shared_ptr<ResourceManager> resource, std::strin
     auto level = m_resource->get_json("levels/" + level_name + ".json");
 
     auto objects = (*level)["interactive"];
-
-    /*std::string fill_name = background_config["fill"];
-
-    auto fill_texture = m_resource->get_texture("graphics/backgrounds/" + fill_name + ".png");
-    fill_texture->setRepeated(true);
-    auto fill_sprite = std::make_shared<sf::Sprite>();
-    fill_sprite->setTexture(*fill_texture);
-    fill_sprite->setTextureRect(sf::IntRect(0, 0, 5000, 1080));
-
-    add_drawable(fill_sprite);*/
-
-
-    //auto objects = background_config["objects"];
     
     for (auto object : objects) {
         std::string name = object["image"];
         auto texture = m_resource->get_texture("graphics/" + name + ".png");
-        //act_pack sprite;//
         auto temp = std::make_shared<sf::Sprite>();
         auto sprite = std::make_shared<act_pack>();
-        /*sprite.sprite.reset();
-        sprite.sprite->setTexture(*texture);*/
         temp->setTexture(*texture);
         int x = object["x"];
         int y = object["y"];
-        //sprite.sprite->move(x * BLOCK_PXSIZE, y * BLOCK_PXSIZE);
-        temp->move(x*BLOCK_PXSIZE, y*BLOCK_PXSIZE);
         temp->setScale(object["size"], object["size"]);
+        temp->move(x*BLOCK_PXSIZE, y*BLOCK_PXSIZE-temp->getGlobalBounds().height);
         sprite->sprite = temp;
-        //sprite->sprite->setOrigin((sprite->sprite->getGlobalBounds().width / 2), (sprite->sprite->getGlobalBounds().height / 2));
-        //sprite->sprite->move((sprite->sprite->getGlobalBounds().width / 2), (sprite->sprite->getGlobalBounds().height / 2));
-        //add_drawable(sprite);
         sprite->function = string_conv(object["function"]);
+        if (sprite->function == mob || sprite->function == bonus)
+        {
+            sprite->value = object["value"];
+        }
         sprite->use = false;
         m_sprites.push_back(sprite);
-        //m_function.push_back((function)object["function"]);
         add_drawable(sprite->sprite);
     }
 }
@@ -84,20 +67,14 @@ colision Interactives::update( sf::FloatRect rect, std::shared_ptr<sf::Text> sco
     bool no_col = true;
     for (auto pack : m_sprites)
     {
-        if (points == -5)
-        {
-            auto texture = m_resource->get_texture("graphics/interactives/point0.png");
-            pack->sprite->setTexture(*texture);
-        }
         if (pack->function == platform)
         {
             if (rect.intersects(pack->sprite->getGlobalBounds()))
             {
-                if ((rect.top ) <= pack->sprite->getGlobalBounds().top - pack->sprite->getGlobalBounds().height+10)
+
+                if ((rect.top +rect.height) <= pack->sprite->getGlobalBounds().top+20)
                 {
-                    col.walk_level = pack->sprite->getGlobalBounds().top-pack->sprite->getGlobalBounds().height;
-                    //col.walk_level = 658 - (BLOCK_PXSIZE * ((SCREEN_Y_BLOCKS)-16));
-        points = 0;
+                    col.walk_level = pack->sprite->getGlobalBounds().top-rect.height+1;
                 }
                 else if (rect.left + rect.width / 2 >= pack->sprite->getGlobalBounds().left + pack->sprite->getGlobalBounds().width / 2)
                 {
@@ -111,7 +88,6 @@ colision Interactives::update( sf::FloatRect rect, std::shared_ptr<sf::Text> sco
                 }
                 no_col = false;
             }
-
         }
         if (pack->function == bonus)
         {
@@ -121,13 +97,9 @@ colision Interactives::update( sf::FloatRect rect, std::shared_ptr<sf::Text> sco
                 {
                     auto texture = m_resource->get_texture("graphics/interactives/invisible.png");
                     pack->sprite->setTexture(*texture);
-                    points++;
+                    points+=pack->value;
                 }
                 pack->use = true;
-            }
-            else
-            {
-                pack->use = false;
             }
 
         }
@@ -138,8 +110,7 @@ colision Interactives::update( sf::FloatRect rect, std::shared_ptr<sf::Text> sco
             {
                 if (pack->use == false)
                 {
-                    points--;
-                    //pack.sprite->setRotation(90);
+                    points-=pack->value;
                 }
                 pack->use = true;
             }
--- src/Interactives.h
+++ src/Interactives.h
@@ -21,16 +21,14 @@ typedef struct
     std::shared_ptr<sf::Sprite> sprite;
     function_enum function;
     bool use;
+    int value;
 }act_pack;
 
 class Interactives : public Object {
 public:
     Interactives(std::shared_ptr<ResourceManager> ressource, std::string level_name);
-    //void update(std::shared_ptr<Character> mainPerson, std::shared_ptr<sf::Text> score);
     colision update( sf::FloatRect rect, std::shared_ptr<sf::Text> score);
 private:
     std::shared_ptr<ResourceManager> m_resource;
-    //std::vector<std::shared_ptr<sf::Sprite>> m_sprites;
-    //std::vector<function> m_function;
     std::vector<std::shared_ptr<act_pack>> m_sprites;
 };
