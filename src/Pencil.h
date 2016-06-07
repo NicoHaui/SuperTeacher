eea06a0d6642d11992e3b74c2e2c2eebc35afeef
Lars Aeschlimann <lars.aeschlimann@students.bfh.ch>
2016-06-07 11:29:06 +0200

    Addition jeter

---
+++ src/Pencil.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Object.h"
+
+#include <string>
+#include <memory>
+#include "ResourceManager.h"
+#include <SFML/Graphics.hpp>
+
+class Pencil : public Object {
+public:
+    Pencil(std::shared_ptr<ResourceManager> ressource, float posx, float posy);
+    void update(void);
+private:
+    std::shared_ptr<ResourceManager> m_resource;
+    std::shared_ptr<sf::Sprite> m_pencil;
+};
